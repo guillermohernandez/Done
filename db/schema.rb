d13a3e38f93ff2a6ff66a24c3828bc9722ad8ce2
Guillermo Hernandez Jr <guillermohernandez@gmail.com>
2015-12-18 00:27:20 -0600

    added state to our tasks. getting activerecord error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206062756) do
+ActiveRecord::Schema.define(version: 20151218050151) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "state",      default: "to_do"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
